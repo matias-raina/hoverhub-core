714e3f3089bf609f23fecbacf77055e289cbd5f6
matias-raina <meraina@estudiantes.unsam.edu.ar>
2025-11-07 17:49:36 -0300

    Add account router with CRUD operations for account management

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,5 @@
 from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
+from app.routers.account import router as account_router
 
-routers = [auth_router, users_router]
+routers = [auth_router, users_router, account_router]
