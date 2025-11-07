fffa48800c72044d26291336052d9a62fb6983b1
Marcos Achaval <marcos.achavalr@gmail.com>
2025-11-07 18:18:46 -0300

    Add Job in inits

--- app/domain/repositories/interfaces/__init__.py
+++ app/domain/repositories/interfaces/__init__.py
@@ -1,4 +1,5 @@
 from app.domain.repositories.interfaces.auth import IAuthRepository
 from app.domain.repositories.interfaces.user import IUserRepository
+from app.domain.repositories.interfaces.job import IJobRepository
 
-__all__ = ["IUserRepository", "IAuthRepository"]
+__all__ = ["IUserRepository", "IAuthRepository", "IJobRepository"]
