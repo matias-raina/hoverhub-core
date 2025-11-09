945fafea0dcedfc157075000420601573ae4eb9c
LucasACH <achaval.lucas@gmail.com>
2025-11-09 12:33:23 -0300

    Add account repository and service dependencies to the configuration. Introduced `get_account_repository` and `get_account_service` functions for dependency injection.

--- app/config/dependencies.py
+++ app/config/dependencies.py
@@ -7,13 +7,17 @@ from sqlmodel import Session
 from app.config.database import get_db
 from app.config.settings import Settings, get_settings
 from app.domain.models.user import User
+from app.domain.repositories.account import AccountRepository
 from app.domain.repositories.auth import AuthRepository
+from app.domain.repositories.interfaces.account import IAccountRepository
 from app.domain.repositories.interfaces.auth import IAuthRepository
 from app.domain.repositories.interfaces.job import IJobRepository
 from app.domain.repositories.interfaces.user import IUserRepository
 from app.domain.repositories.job import JobRepository
 from app.domain.repositories.user import UserRepository
+from app.services.account import AccountService
 from app.services.auth import AuthService
+from app.services.interfaces.account import IAccountService
 from app.services.interfaces.auth import IAuthService
 from app.services.interfaces.job import IJobService
 from app.services.interfaces.user import IUserService
@@ -47,9 +51,16 @@ def get_job_repository(
     return JobRepository(session)
 
 
+def get_account_repository(
+    session: SessionDep,
+) -> IAccountRepository:
+    return AccountRepository(session)
+
+
 UserRepositoryDep = Annotated[IUserRepository, Depends(get_user_repository)]
 AuthRepositoryDep = Annotated[IAuthRepository, Depends(get_auth_repository)]
 JobRepositoryDep = Annotated[IJobRepository, Depends(get_job_repository)]
+AccountRepositoryDep = Annotated[IAccountRepository, Depends(get_account_repository)]
 
 
 # Service dependencies
@@ -72,9 +83,17 @@ def get_job_service(
     return JobService(job_repository)
 
 
+def get_account_service(
+    account_repository: AccountRepositoryDep,
+    user_repository: UserRepositoryDep,
+) -> IAccountService:
+    return AccountService(account_repository, user_repository)
+
+
 AuthServiceDep = Annotated[IAuthService, Depends(get_auth_service)]
 UserServiceDep = Annotated[IUserService, Depends(get_user_service)]
 JobServiceDep = Annotated[IJobService, Depends(get_job_service)]
+AccountServiceDep = Annotated[IAccountService, Depends(get_account_service)]
 
 
 # Authentication dependency - Gets authenticated user from JWT token
