983d4903639fb496755f1dbbc19adbed465c55ea
LucasACH <achaval.lucas@gmail.com>
2025-11-10 11:12:38 -0300

    Refactor repository and service imports for better organization
    - Consolidated import statements in dependencies.py for improved readability.
    - Updated __init__.py files to include SessionRepository and ISessionRepository in the exported members.
    - Created a new interfaces/__init__.py file to organize service interfaces.
    - Added docstrings to repository and service getter functions for clarity.

--- app/domain/repositories/__init__.py
+++ app/domain/repositories/__init__.py
@@ -1,7 +1,13 @@
-from app.domain.repositories.auth import AuthRepository
-from app.domain.repositories.user import UserRepository
-from app.domain.repositories.job import JobRepository
 from app.domain.repositories.account import AccountRepository
+from app.domain.repositories.auth import AuthRepository
+from app.domain.repositories.job import JobRepository
+from app.domain.repositories.session import SessionRepository
+from app.domain.repositories.user import UserRepository
 
-__all__ = ["UserRepository", "AuthRepository",
-           "JobRepository", "AccountRepository"]
+__all__ = [
+    "UserRepository",
+    "AuthRepository",
+    "JobRepository",
+    "AccountRepository",
+    "SessionRepository",
+]
