512aa319434992ad29ea97584606a3f56ef8d502
LucasACH <achaval.lucas@gmail.com>
2025-11-09 23:47:27 -0300

    Update __init__.py to include UserSession in the domain models export

--- app/domain/models/__init__.py
+++ app/domain/models/__init__.py
@@ -1,5 +1,6 @@
 from app.domain.models.user import User
 from app.domain.models.job import Job
 from app.domain.models.account import Account
+from app.domain.models.session import UserSession
 
-__all__ = ["User", "Job", "Account"]
+__all__ = ["User", "Job", "Account", "UserSession"]
