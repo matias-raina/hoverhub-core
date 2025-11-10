55f70835244ae9604b33f5b331d1baebbfc00ddb
Marcos Achaval <marcos.achavalr@gmail.com>
2025-11-10 17:07:12 -0300

    Implement Favorite model and repository with CRUD operations and relationships

---
+++ app/services/interfaces/favorite.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from uuid import UUID
+
+from app.domain.models.favorite import Favorite
+# from app.dto.favorite import CreateFavoriteDto
